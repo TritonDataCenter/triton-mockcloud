f2901cba237b02cc8ee3be481cba14e83dde963e
Josh Wilsdon <jwilsdon@joyent.com>
2015-09-02 16:37:45 -0700

    fix idempotency

--- src/init.sh
+++ src/init.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o xtrace
 
 # replace sysinfo with our version
-umount -f /usr/bin/sysinfo || /bin/true
+umount /usr/bin/sysinfo || /bin/true
 mount -F lofs /opt/smartdc/mockcloud/mocks/sysinfo.js /usr/bin/sysinfo
 
 # create disks.json from sysinfo, used by disklayout when doing setup
@@ -18,15 +18,15 @@ mount -F lofs /opt/smartdc/mockcloud/mocks/sysinfo.js /usr/bin/sysinfo
 # Create /opt/smartdc/agents/lib so Ur scripts don't think we're 6.5
 mkdir -p /opt/smartdc/agents/lib
 
-# make config.sh read config from correct place
-mkdir -p /opt/smartdc/mockcloud/tmp
-cp /lib/sdc/config.sh /opt/smartdc/mockcloud/tmp/config.sh
-
 # pretend like agents install is going to work
 mkdir -p /opt/smartdc/agents/bin
 cp /opt/smartdc/mockcloud/mocks/ping-agent.sh /opt/smartdc/agents/bin/ping-agent
 
 if [[ -z $(grep MOCKCN_SERVER_UUID /lib/sdc/config.sh) ]]; then
+# make config.sh read config from correct place
+mkdir -p /opt/smartdc/mockcloud/tmp
+cp /lib/sdc/config.sh /opt/smartdc/mockcloud/tmp/config.sh
+
 /opt/local/bin/patch /opt/smartdc/mockcloud/tmp/config.sh <<"EOF"
 --- /lib/sdc/config.sh  2013-11-10 07:48:35.629886000 +0000
 +++ config.sh   2013-11-13 21:41:04.651518490 +0000
@@ -58,29 +58,29 @@ mount -F lofs /opt/smartdc/mockcloud/tmp/config.sh /lib/sdc/config.sh
 fi
 
 # replace disklayout + disklist with our version
-umount -f /usr/bin/disklayout || /bin/true
+umount /usr/bin/disklayout || /bin/true
 mount -F lofs /opt/smartdc/mockcloud/mocks/disklayout.js /usr/bin/disklayout
-umount -f /usr/bin/disklist || /bin/true
+umount /usr/bin/disklist || /bin/true
 mount -F lofs /opt/smartdc/mockcloud/mocks/disklist.js /usr/bin/disklist
 
 # mock out vmadm required bits
-umount -f /usr/bin/onlyif.sh || /bin/true
+umount /usr/node/node_modules/onlyif.js || /bin/true
 mount -F lofs /opt/smartdc/mockcloud/mocks/onlyif.js /usr/node/node_modules/onlyif.js
-umount -f /usr/vm/node_modules/vmload/index.js
+umount /usr/vm/node_modules/vmload/index.js || /bin/true
 mount -F lofs /opt/smartdc/mockcloud/mocks/vmload.js /usr/vm/node_modules/vmload/index.js
-umount -f /usr/vm/node_modules/VM.js
+umount /usr/vm/node_modules/VM.js || /bin/true
 mount -F lofs /opt/smartdc/mockcloud/mocks/VM.js /usr/vm/node_modules/VM.js
 
 # replace zoneevent
-umount -f /usr/vm/sbin/zoneevent
+umount /usr/vm/sbin/zoneevent || /bin/true
 mount -F lofs /opt/smartdc/mockcloud/mocks/zoneevent.js /usr/vm/sbin/zoneevent
 
 # replace z* tools with our versions
-umount -f /usr/sbin/zoneadm
+umount /usr/sbin/zoneadm || /bin/true
 mount -F lofs /opt/smartdc/mockcloud/mocks/zoneadm.sh /usr/sbin/zoneadm
-umount -f /usr/sbin/zfs
+umount /usr/sbin/zfs || /bin/true
 mount -F lofs /opt/smartdc/mockcloud/mocks/zfs.sh /usr/sbin/zfs
-umount -f /usr/sbin/zpool
+umount /usr/sbin/zpool || /bin/true
 mount -F lofs /opt/smartdc/mockcloud/mocks/zpool.sh /usr/sbin/zpool
 
 if [[ -n $(svcs -a | grep mock-agent) ]]; then
